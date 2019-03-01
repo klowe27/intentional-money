d2d5a9634c1e801fecafb4cb9581118b46500040
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 15:56:44 -0800

    Add AddAccountForm component

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import TransactionList from './TransactionList';
+import AddAccountForm from './AddAccountForm';
+import Button from './Button';
 
-function Accounts(){
-  return (
-    <div>
-      <TransactionList/>
-    </div>
-  )
+class Accounts extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showAddAccountForm: false,
+    }
+    this.toggleAccountForm = this.toggleAccountForm.bind(this);
+  }
+  
+  toggleAccountForm(){
+    this.setState({showAddAccountForm: !this.state.showAddAccountForm});
+  }
+  
+  render() {
+    return (
+      <div><h2>Accounts</h2>
+        <TransactionList/>
+        <Button 
+          action={this.toggleAccountForm}
+          name='+ account'
+        /> 
+        <AddAccountForm
+          showAddAccountForm={this.state.showAddAccountForm}
+          toggleAccountForm={this.toggleAccountForm}
+        />
+      </div>
+    )
+  }
 }
 
 export default Accounts;
---
+++ src/components/AddAccountForm.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AddAccountForm({showAddAccountForm, toggleAccountForm}) {
+  let _name = null;
+  let _balance = null;
+  
+  function handleAddCategory(e){
+    e.preventDefault();
+    console.log(_name.value);
+    console.log(_balance.value);
+    toggleAccountForm();
+  }
+  
+  if (!showAddAccountForm){
+    return(
+      null
+    )
+  } else {
+    return(
+      <div className='modal-background'> 
+        <form onSubmit={toggleAccountForm} className='form'>
+          <span className='close' onClick={toggleAccountForm}>x</span>
+          <h2>Add Account</h2>
+          <div className='form-group'>
+            <label from='name'>Account name</label>
+            <input
+              type='text'
+              placeholder='Checking'
+              ref={(input)=>{_name=input;}}
+            />
+          </div>
+          <div className='form-group'>
+            <label from='balance'>Current balance</label>
+            <input
+              type='number'
+              ref={(input)=>{_balance=input;}}
+            />
+          </div>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+AddAccountForm.propTypes = {
+  showAddAccountForm: PropTypes.bool,
+  toggleAccountForm: PropTypes.func
+};
+
+export default AddAccountForm;
