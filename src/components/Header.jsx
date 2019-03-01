00cbbcef5c771183a75810ff3cc6528b9c16e9d4
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 09:03:10 -0800

    Add state and method for menu toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Button from './Button'
 import { Link } from 'react-router-dom';
+import './assets/styles/Header.css';
 
 class Header extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      showAddTransactionForm: false
+      showAddTransactionForm: false,
+      showMenu: false
     }
     this.toggleAddTransactionForm = this.toggleAddTransactionForm.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  
+  toggleMenu(){
+    this.setState({showMenu: !this.state.showMenu})
   }
   
   toggleAddTransactionForm(){
@@ -18,6 +25,10 @@ class Header extends React.Component {
   render() {
     return (
       <div>
+        <div onClick={this.toggleMenu} className='menuTabBox'>
+          <div className='menuTab tab1'></div>
+          <div className='menuTab tab2'></div>
+        </div>
         <h1>Intentional Money</h1>
         <Button 
           action={this.toggleAddTransactionForm}
