9bd5a82b9119257b2dd5bf115077be5fbce0d1bc
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 08:40:17 -0800

    Add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function Button({action, name}) {
+  return (
+    <button onClick={action}>{name}</button>
+  )
+}
+
+Button.propTypes = {
+  action: PropTypes.func,
+  name: PropTypes.string
+}
+
+export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import Button from './Button'
 import { Link } from 'react-router-dom';
 
-function Header(){
-  return (
-    <div>
-      <h1>Intentional Money</h1>
-    </div>
-  );
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showAddTransactionForm: false
+    }
+    this.toggleAddTransactionForm = this.toggleAddTransactionForm.bind(this);
+  }
+  
+  toggleAddTransactionForm(){
+    this.setState({showAddTransactionForm: !this.state.showAddTransactionForm})
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Intentional Money</h1>
+        <Button 
+          action={this.toggleAddTransactionForm}
+          name="+ transaction"
+        />
+      </div>
+    );
+  }
 }
 
 export default Header;
