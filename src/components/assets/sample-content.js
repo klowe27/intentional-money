9ca51b02346f251c4b4b4f8a2cc9e9eca255971b
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 14:57:06 -0800

    Add sample transaction data

--- src/components/assets/sample-content.js
+++ src/components/assets/sample-content.js
@@ -24,3 +24,56 @@ export const categoryListData = [
     remaining: '0'
   }
 ]
+
+export const sampleTransactions = [
+  {
+    date: '2019-05-24',
+    vendor: 'Trade Joes',
+    note: 'Sample note',
+    amount: 35,
+    category: 'Grocies',
+    account: 'Checking',
+    expense: true,
+    cleared: true
+  },
+  {
+    date: '2019-05-20',
+    vendor: 'Ting',
+    note: 'Sample note',
+    amount: 35,
+    category: 'Phone',
+    account: 'Credit card',
+    expense: true,
+    cleared: true
+  },
+  {
+    date: '2019-05-1',
+    vendor: 'Propery Management',
+    note: 'Sample note',
+    amount: 600,
+    category: 'Rent',
+    account: 'Checking',
+    expense: true,
+    cleared: true
+  },
+  {
+    date: '2019-05-5',
+    vendor: 'Amazon',
+    note: 'Sample note',
+    amount: 35,
+    category: 'Spending money',
+    account: 'Credit Cart',
+    expense: true,
+    cleared: false
+  },
+  {
+    date: '2019-05-2',
+    vendor: 'Work',
+    note: 'Sample note',
+    amount: 1000,
+    category: 'Income',
+    account: 'Checking',
+    expense: false,
+    cleared: true
+  }
+]
