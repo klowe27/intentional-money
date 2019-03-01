116d22778a20cf7977e184cc7ebd77d86b87b634
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 13:51:32 -0800

    Add AddCategoryForm and CategoryItem components

---
+++ src/components/AddCategoryForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AddCategoryForm({showAddCategoryForm, toggleCategoryForm}) {
+  let _name = null;
+  let _budget = null;
+  
+  function handleAddCategory(e){
+    e.preventDefault();
+    console.log(_name.value);
+    console.log(_budget.value);
+
+    toggleCategoryForm();
+  }
+  
+  if (!showAddCategoryForm){
+    return(
+      null
+    )
+  } else {
+    return(
+      <div className='modal-background'> 
+        <form onSubmit={handleAddCategory} className='form'>
+          <span className='close' onClick={toggleCategoryForm}>x</span>
+          <h2>Add Category</h2>
+          <div className='form-group'>
+            <label from='name'>Category name</label>
+            <input
+              type='text'
+              placeholder='Grocies'
+              ref={(input)=>{_name=input;}}
+            />
+          </div>
+          <div className='form-group'>
+            <label from='budget'>Budget</label>
+            <input
+              type='number'
+              ref={(input)=>{_budget=input;}}
+            />
+          </div>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+AddCategoryForm.propTypes = {
+  showAddCategoryForm: PropTypes.boolean,
+  toggleCategoryForm: PropTypes.func
+};
+
+export default AddCategoryForm;
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoryItem() {
+  return(
+    <div>CategoryItem</div>
+  )
+}
+
+export default CategoryItem;
