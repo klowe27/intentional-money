29de87a6f9a377f9032825d9f7d30afaf40433ce
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-01 09:25:50 -0800

    Add conditional rendering for menu and basic styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import './assets/styles/Header.css';
+import './assets/styles/Menu.css';
 
 
-function Menu() {
-  
-  return(
-    <div className='menu'>
-      <div className='menu-top'></div>
-      <Link to='/budget'><div className='menu-item'>Budget</div></Link>
-      <Link to='/accounts'><div className='menu-item'>Accounts</div></Link>
-    </div>
-  )
+function Menu({showMenu}) {
+  if (!showMenu) {
+    return (
+      null
+    )
+  } else {
+    return (
+      <div className='menu'>
+        <div className='menu-top'></div>
+        <Link to='/budget'><div className='menu-item'>Budget</div></Link>
+        <Link to='/accounts'><div className='menu-item'>Accounts</div></Link>
+      </div>
+    )
+  }  
+}
+
+Menu.propTypes = {
+  showMenu: PropTypes.boolean
 }
 
 export default Menu;
