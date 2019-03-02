e8490d1358b2f8d116cea418f0462b3aa5ea394e
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-03-02 11:05:29 -0800

    Add image and CoinList

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import CoinList from './CoinList'
+import Pig from './assets/images/pig.png';
+import PigBelly from './assets/images/pig-belly.png';
+import './assets/styles/Homepage.css';
 
 function Homepage(){
   return (
-    <div>Homepage</div>
+    <div>
+      <CoinList/>
+      <img src={Pig} className='piggy'/>
+      <img src={PigBelly} className='piggy belly'/>
+    </div>
   );
 }
 
